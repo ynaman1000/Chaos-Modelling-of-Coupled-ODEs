3c5e67b4faa87754f9b976c3f209485fa6960796
RIYA <rsriya383@gmail.com>
2020-06-03 12:23:38 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -41,19 +41,19 @@ def implct(stp, theta1, theta2, p1, p2):
 
         theta1 = theta1_prev + stp*theta1_dot_fun(theta1_iter, theta2_iter, p1_iter, p2_iter)
         theta2 = theta2_prev + stp*theta2_dot_fun(theta1_iter, theta2_iter, p1_iter, p2_iter)
-        p1 = p_1_prev + stp*p1_dot_fun(theta1_iter, theta2_iter, p1_iter, p2_iter)
-        p2 = p_2_prev + stp*p2_dot_fun(theta1_iter, theta2_iter, p1_iter, p2_iter)
+        p1 = p1_prev + stp*p1_dot_fun(theta1_iter, theta2_iter, p1_iter, p2_iter)
+        p2 = p2_prev + stp*p2_dot_fun(theta1_iter, theta2_iter, p1_iter, p2_iter)
 
         while hgh_rel_err([theta1, theta2, p1, p2], [theta1_iter, theta2_iter, p1_iter, p2_iter]):
-            theta1_iter = theta_1*r + theta1*(1-r)
-            theta2_iter = theta_2*r + theta2*(1-r)
-            p1_iter = p_1*r + p1*(1-r)
-            p2_iter = p_2*r + p2*(1-r)
+            theta1_iter = theta1_prev*r + theta1*(1-r)
+            theta2_iter = theta2_prev*r + theta2*(1-r)
+            p1_iter = p1_prev*r + p1*(1-r)
+            p2_iter = p2_prev*r + p2*(1-r)
 
-            theta1 = theta_1_prev + stp*theta1_dot_fun(theta1_iter, theta2_iter, p1_iter, p2_iter)
-            theta2 = theta_2_prev + stp*theta2_dot_fun(theta1_iter, theta2_iter, p1_iter, p2_iter)
-            p1 = p_1_prev + stp*p1_dot_fun(theta1_iter, theta2_iter, p1_iter, p2_iter)
-            p2 = p_2_prev + stp*p2_dot_fun(theta1_iter, theta2_iter, p1_iter, p2_iter)
+            theta1 = theta1_prev + stp*theta1_dot_fun(theta1_iter, theta2_iter, p1_iter, p2_iter)
+            theta2 = theta2_prev + stp*theta2_dot_fun(theta1_iter, theta2_iter, p1_iter, p2_iter)
+            p1 = p1_prev + stp*p1_dot_fun(theta1_iter, theta2_iter, p1_iter, p2_iter)
+            p2 = p2_prev + stp*p2_dot_fun(theta1_iter, theta2_iter, p1_iter, p2_iter)
 
         theta1_dot = theta1_dot_fun(theta1, theta2, p1, p2)
         theta2_dot = theta2_dot_fun(theta1, theta2, p1, p2)

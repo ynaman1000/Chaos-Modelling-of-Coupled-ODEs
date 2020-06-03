0fb74a0128584aaf55f627a04c0b40448c0c5021
RIYA <rsriya383@gmail.com>
2020-06-03 09:35:29 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import functions as fun
+
+theta1_i = np.array([1,1,1.0012,1,1,1,1,1.0012])*np.pi/2
+theta2_i = np.array([1,0.9988,1,1,1,1,1,0.9988])*np.pi/2
+p1_i = np.array([0,0,0,0.01,0,-0.01,0,0.01])
+p2_i = np.array([0,0,0,0,0.01,0,-0.01,-0.01])

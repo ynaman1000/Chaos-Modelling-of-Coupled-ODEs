23d2c742abb7afcc0cc40380879aeadf8f813599
RIYA <rsriya383@gmail.com>
2020-06-03 13:20:05 +0530

    Added plotting functions

--- main.py
+++ main.py
@@ -36,4 +36,25 @@ for j in range (0,8):
             if solver == "RK6":
               theta1, theta2, p1, p2, theta1_dot, theta2_dot = rk6(stp, theta1, theta2, p1, p2)
     states_arr.append([t, theta1, theta2, theta1_dot, theta2_dot, p1, p2])
-  
+    states = np.asarray(states_arr) 
+    X1 = []
+    X2 = []
+    Y1 = []
+    Y2 = []
+    for i in np.size(states[:,0]): #size of a column of states
+        X1.append(l*cos(states[i,1])/2) 
+        Y1.append(-l*sin(states[i,1])/2)
+        X2.append(2*X1 + l*cos(states[i,1])/2)
+        Y2.append(-2*Y1 - l*sin(states[i,1])/2)
+    fig, ax1 = plt.subplots()   
+    fig, ax2 = plt.subplots()    
+    ax1.plot(X1,Y1,label=str(j))
+    ax2.plot(X2,Y2,label=str(j))
+ax1.xlabel("X-axis")
+ax2.xlabel("X-axis")
+ax1.ylabel("Y-axis")
+ax2.ylabel("Y-axis")
+ax1.title("trajectory of 1st rod center")
+ax2.title("trajectory of 2nd rod center")
+ax1.show()
+ax2.show()

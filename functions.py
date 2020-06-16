631069e80a36e04cf0f93c759eb8639502580002
Naman Yadav <ynaman1000@gmail.com>
2020-06-16 10:24:54 +0530

    "rearranged things"

--- functions.py
+++ functions.py
@@ -1,76 +1,105 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-m = 1
-l = 1
 g = 9.81
 
 def hgh_rel_err(acc, app, rel_err_tol):
     # print("inner_loop_err: ", np.amax(np.divide(np.absolute(acc-app), (acc+1e-12))))
     return np.amax(np.divide(np.absolute(acc-app), (acc+1e-12))) > rel_err_tol
 
-def theta1_dot_fun(theta1, theta2, p1, p2):
+def theta1_dot_fun(theta1, theta2, p1, p2, m, l):
     return (6/(m*(l**2))) * (2*p1 - 3*np.cos(theta1-theta2)*p2) / (16 - 9*(np.cos(theta1-theta2)**2))
 
-def theta2_dot_fun(theta1, theta2, p1, p2):
+def theta2_dot_fun(theta1, theta2, p1, p2, m, l):
     return (6/(m*(l**2))) * (8*p2 - 3*np.cos(theta1-theta2)*p1) / (16 - 9*(np.cos(theta1-theta2)**2))
 
-def p1_dot_fun(theta1, theta2, p1, p2):
+def p1_dot_fun(theta1, theta2, p1, p2, m, l):
     return ((-m*(l**2))/2) * \
-        (theta1_dot_fun(theta1, theta2, p1, p2)*theta2_dot_fun(theta1, theta2, p1, p2)*np.sin(theta1-theta2) + (3*g*np.sin(theta1))/l)
+        (theta1_dot_fun(theta1, theta2, p1, p2, m, l)*theta2_dot_fun(theta1, theta2, p1, p2, m, l)*np.sin(theta1-theta2) + (3*g*np.sin(theta1))/l)
 
-def p2_dot_fun(theta1, theta2, p1, p2):
+def p2_dot_fun(theta1, theta2, p1, p2, m, l):
     return ((-m*(l**2))/2) * \
-        (-theta1_dot_fun(theta1, theta2, p1, p2)*theta2_dot_fun(theta1, theta2, p1, p2)*np.sin(theta1-theta2) + (g*np.sin(theta2))/l)
+        (-theta1_dot_fun(theta1, theta2, p1, p2, m, l)*theta2_dot_fun(theta1, theta2, p1, p2, m, l)*np.sin(theta1-theta2) + (g*np.sin(theta2))/l)
 
-def explct(stp, theta1, theta2, p1, p2):
-            theta1 = theta1 + stp*theta1_dot_fun(theta1, theta2, p1, p2)
-            theta2 = theta2 + stp*theta2_dot_fun(theta1, theta2, p1, p2)
-            p1 = p1 + stp*p1_dot_fun(theta1, theta2, p1, p2)
-            p2 = p2 + stp*p2_dot_fun(theta1, theta2, p1, p2)
-            # theta1_dot = theta1_dot_fun(theta1, theta2, p1, p2)
-            # theta2_dot = theta2_dot_fun(theta1, theta2, p1, p2)
-            return theta1, theta2, p1, p2
+def explct(stp, theta1, theta2, p1, p2, m, l):
+    theta1 = theta1 + stp*theta1_dot_fun(theta1, theta2, p1, p2, m, l)
+    theta2 = theta2 + stp*theta2_dot_fun(theta1, theta2, p1, p2, m, l)
+    p1 = p1 + stp*p1_dot_fun(theta1, theta2, p1, p2, m, l)
+    p2 = p2 + stp*p2_dot_fun(theta1, theta2, p1, p2, m, l)
+    # theta1_dot = theta1_dot_fun(theta1, theta2, p1, p2)
+    # theta2_dot = theta2_dot_fun(theta1, theta2, p1, p2)
+    return theta1, theta2, p1, p2
 
-def implct(stp, theta1, theta2, p1, p2, rel_err_tol=1e-4, r=0):
-        theta1_iter = theta1_prev = theta1
-        theta2_iter = theta2_prev = theta2
-        p1_iter = p1_prev = p1
-        p2_iter = p2_prev = p2
-        # theta1_dot_iter = theta1_dot_prev = theta1_dot
-        # theta2_dot_iter = theta2_dot_prev = theta2_dot
+def implct(stp, theta1, theta2, p1, p2, m, l, rel_err_tol, r):
+    theta1_iter = theta1_prev = theta1
+    theta2_iter = theta2_prev = theta2
+    p1_iter = p1_prev = p1
+    p2_iter = p2_prev = p2
+    # theta1_dot_iter = theta1_dot_prev = theta1_dot
+    # theta2_dot_iter = theta2_dot_prev = theta2_dot
 
-        theta1 = theta1_prev + stp*theta1_dot_fun(theta1_iter, theta2_iter, p1_iter, p2_iter)
-        theta2 = theta2_prev + stp*theta2_dot_fun(theta1_iter, theta2_iter, p1_iter, p2_iter)
-        p1 = p1_prev + stp*p1_dot_fun(theta1_iter, theta2_iter, p1_iter, p2_iter)
-        p2 = p2_prev + stp*p2_dot_fun(theta1_iter, theta2_iter, p1_iter, p2_iter)
-        # theta1_dot = theta1_dot_fun(theta1_iter, theta2_iter, p1_iter, p2_iter)
-        # theta2_dot = theta2_dot_fun(theta1_iter, theta2_iter, p1_iter, p2_iter)
-        # theta1_dot = theta1_dot_fun(theta1, theta2, p1, p2)
-        # theta2_dot = theta2_dot_fun(theta1, theta2, p1, p2)
+    theta1 = theta1_prev + stp*theta1_dot_fun(theta1_iter, theta2_iter, p1_iter, p2_iter, m, l)
+    theta2 = theta2_prev + stp*theta2_dot_fun(theta1_iter, theta2_iter, p1_iter, p2_iter, m, l)
+    p1 = p1_prev + stp*p1_dot_fun(theta1_iter, theta2_iter, p1_iter, p2_iter, m, l)
+    p2 = p2_prev + stp*p2_dot_fun(theta1_iter, theta2_iter, p1_iter, p2_iter, m, l)
+    # theta1_dot = theta1_dot_fun(theta1_iter, theta2_iter, p1_iter, p2_iter, m, l)
+    # theta2_dot = theta2_dot_fun(theta1_iter, theta2_iter, p1_iter, p2_iter, m, l)
+    # theta1_dot = theta1_dot_fun(theta1, theta2, p1, p2, m, l)
+    # theta2_dot = theta2_dot_fun(theta1, theta2, p1, p2, m, l)
 
-        # while hgh_rel_err(np.array([theta1, theta2, p1, p2, theta1_dot, theta2_dot]),\
+    # while hgh_rel_err(np.array([theta1, theta2, p1, p2, theta1_dot, theta2_dot]),\
         #                   np.array([theta1_iter, theta2_iter, p1_iter, p2_iter, theta1_dot_iter, theta2_dot_iter]),\
         #                   rel_err_tol):
-        while hgh_rel_err(np.array([theta1, theta2, p1, p2]),\
-                          np.array([theta1_iter, theta2_iter, p1_iter, p2_iter]),\
-                          rel_err_tol):
-            # print("inner_loop", p1, p1_iter)
-            theta1_iter = theta1_prev*r + theta1*(1-r)
-            theta2_iter = theta2_prev*r + theta2*(1-r)
-            p1_iter = p1_prev*r + p1*(1-r)
-            p2_iter = p2_prev*r + p2*(1-r)
-            # theta1_dot_iter = theta1_dot_prev*r + theta1_dot*(1-r)
-            # theta2_dot_iter = theta2_dot_prev*r + theta2_dot*(1-r)
+    while hgh_rel_err(np.array([theta1, theta2, p1, p2]),\
+                      np.array([theta1_iter, theta2_iter, p1_iter, p2_iter]),\
+                      rel_err_tol):
+        # print("inner_loop", p1, p1_iter)
+        theta1_iter = theta1_prev*r + theta1*(1-r)
+        theta2_iter = theta2_prev*r + theta2*(1-r)
+        p1_iter = p1_prev*r + p1*(1-r)
+        p2_iter = p2_prev*r + p2*(1-r)
+        # theta1_dot_iter = theta1_dot_prev*r + theta1_dot*(1-r)
+        # theta2_dot_iter = theta2_dot_prev*r + theta2_dot*(1-r)
 
-            theta1 = theta1_prev + stp*theta1_dot_fun(theta1_iter, theta2_iter, p1_iter, p2_iter)
-            theta2 = theta2_prev + stp*theta2_dot_fun(theta1_iter, theta2_iter, p1_iter, p2_iter)
-            p1 = p1_prev + stp*p1_dot_fun(theta1_iter, theta2_iter, p1_iter, p2_iter)
-            p2 = p2_prev + stp*p2_dot_fun(theta1_iter, theta2_iter, p1_iter, p2_iter)
-            # theta1_dot = theta1_dot_fun(theta1, theta2, p1, p2)
-            # theta2_dot = theta2_dot_fun(theta1, theta2, p1, p2)
-            
-        return theta1, theta2, p1, p2
+        theta1 = theta1_prev + stp*theta1_dot_fun(theta1_iter, theta2_iter, p1_iter, p2_iter, m, l)
+        theta2 = theta2_prev + stp*theta2_dot_fun(theta1_iter, theta2_iter, p1_iter, p2_iter, m, l)
+        p1 = p1_prev + stp*p1_dot_fun(theta1_iter, theta2_iter, p1_iter, p2_iter, m, l)
+        p2 = p2_prev + stp*p2_dot_fun(theta1_iter, theta2_iter, p1_iter, p2_iter, m, l)
+        # theta1_dot = theta1_dot_fun(theta1, theta2, p1, p2, m, l)
+        # theta2_dot = theta2_dot_fun(theta1, theta2, p1, p2, m, l)
+
+    return theta1, theta2, p1, p2
+
+def solve(theta1, theta2, p1, p2, m, l, stp, ti, tf, solver, rel_err_tol=1e-6, r=0.0):
+    states_arr = []
+    
+    # theta1_dot = theta1_dot_i[j]
+    # theta2_dot = theta2_dot_i[j]
+    t = ti
+
+    while (t < tf):
+        # print(t)
+        theta1_dot = theta1_dot_fun(theta1, theta2, p1, p2, m, l)
+        theta2_dot = theta2_dot_fun(theta1, theta2, p1, p2, m, l)
+        states_arr.append([t, theta1, theta2, theta1_dot, theta2_dot, p1, p2])
+        t += stp
+        if solver == "E":
+            theta1, theta2, p1, p2 = explct(stp, theta1, theta2, p1, p2, m, l)
+        elif solver == "I":
+            theta1, theta2, p1, p2 = implct(stp, theta1, theta2, p1, p2, m, l, rel_err_tol, r)
+        # elif solver == "RK4":
+        #     theta1, theta2, p1, p2 = rk4(stp, theta1, theta2, p1, p2)
+        # elif solver == "RK6":
+        #     theta1, theta2, p1, p2 = rk6(stp, theta1, theta2, p1, p2)
+        theta1_dot = theta1_dot_fun(theta1, theta2, p1, p2, m, l)
+        theta2_dot = theta2_dot_fun(theta1, theta2, p1, p2, m, l)
+        states_arr.append([t, theta1, theta2, theta1_dot, theta2_dot, p1, p2])
+        # print(states_arr)
+        # print(len(states_arr), len(states_arr[0]))
+        states = np.asarray(states_arr)
+        # print(states.shape)
+
+        return states
 
 # f = open("out.txt", "r")
 # dat = []
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from functions import *
 
-ni = 4
 theta1_i = np.array([np.pi, np.pi+0.01, np.pi, np.pi+0.1])
 theta2_i = np.array([np.pi+0.01, np.pi, np.pi-0.1, np.pi])
 p1_i = np.array([0.0, 0.0, 0.0, 0.0])
 p2_i = np.array([0.0, 0.0, 0.0, 0.0])
+ni = theta1_i.size
+ms = np.array([1.0])
+ls = np.array([1.0])
 
 stp = 0.001
 ti = 0.0
@@ -13,74 +15,48 @@ tf = 2
 #set solver to 
 #"I" if implicit is used
 #"E" is explicit is used
-#"rk4" if RK4 is used
-#"rk6" if RK6 is used
-solver = "I"
+#(not implemented) "rk4" if RK4 is used
+#(not implemented) "rk6" if RK6 is used
+solver = "E"
 if (solver == "I"):             # setting up optional variables
-    rel_err_tol = 1e-6                  # default value is 1e-4
+    rel_err_tol = 1e-6                  # default value is 1e-6
     r = 0.0                    # default value is 0
 
-for j in range (ni):
-    print("initial conditions: ", j+1, "/", ni)
-    states_arr = []
-    theta1 = theta1_i[j]
-    theta2 = theta2_i[j]
-    p1 = p1_i[j]
-    p2 = p2_i[j]
-    # theta1_dot = theta1_dot_i[j]
-    # theta2_dot = theta2_dot_i[j]
-    t = ti
-    
-    while (t < tf):
-        # print(t)
-        theta1_dot = theta1_dot_fun(theta1, theta2, p1, p2)
-        theta2_dot = theta2_dot_fun(theta1, theta2, p1, p2)
-        states_arr.append([t, theta1, theta2, theta1_dot, theta2_dot, p1, p2])
-        t += stp
-        if solver == "E":
-            theta1, theta2, p1, p2 = explct(stp, theta1, theta2, p1, p2)
-        elif solver == "I":
-            theta1, theta2, p1, p2 = implct(stp, theta1, theta2, p1, p2, rel_err_tol, r)
-        elif solver == "RK4":
-            theta1, theta2, p1, p2 = rk4(stp, theta1, theta2, p1, p2)
-        elif solver == "RK6":
-            theta1, theta2, p1, p2 = rk6(stp, theta1, theta2, p1, p2)
-    theta1_dot = theta1_dot_fun(theta1, theta2, p1, p2)
-    theta2_dot = theta2_dot_fun(theta1, theta2, p1, p2)
-    states_arr.append([t, theta1, theta2, theta1_dot, theta2_dot, p1, p2])
-    # print(states_arr)
-    # print(len(states_arr), len(states_arr[0]))
-    states = np.asarray(states_arr)
-    # print(states.shape)
 
-    print("plotting...")
-    X1 = []
-    X2 = []
-    Y1 = []
-    Y2 = []
-    
-    for i in range(np.size(states[:,0])):
-        X1.append(l*np.sin(states[i,1])/2)
-        Y1.append(-l*np.cos(states[i,1])/2) 
-        X2.append(2*X1[i] + l*np.sin(states[i,2])/2)
-        Y2.append(2*Y1[i] - l*np.cos(states[i,2])/2)
-        
-    # fig, ax1 = plt.subplots()   
-    # fig, ax2 = plt.subplots()
-    # print(len(X1), len(X2), len(Y1), len(Y2))
-    # ax1.plot(states[:,1], states[:,2], label=str(j+1))
-    # ax1.plot(X1,Y1,label=str(j+1))
-    plt.plot(X2,Y2,label=str(j+1))
+for j in range(ms.size):
+    for k in range(ls.size):
+        for i in range (ni):
+            print("initial conditions: ", i+1, "/", ni)
+            states = solve(theta1_i[i], theta2_i[i], p1_i[i], p2_i[i], ms[j], ls[k], stp, ti, tf, solver)
 
-# ax1.xlabel("X-axis")
-# ax2.xlabel("X-axis")
-# ax1.ylabel("Y-axis")
-# ax2.ylabel("Y-axis")
-# ax1.title("trajectory of 1st rod center")
-# ax2.title("trajectory of 2nd rod center")
-# ax1.show()
-# ax2.show()
-plt.xlabel("X-axis")
-plt.ylabel("Y-axis")
-plt.title("trajectory of the centre of 2nd link")
-plt.show()
+            print("plotting...")
+            X1 = []
+            X2 = []
+            Y1 = []
+            Y2 = []
+
+            for xi in range(np.size(states[:,0])):
+                X1.append(ls[k]*np.sin(states[xi,1])/2)
+                Y1.append(-ls[k]*np.cos(states[xi,1])/2) 
+                X2.append(2*X1[xi] + ls[k]*np.sin(states[xi,2])/2)
+                Y2.append(2*Y1[xi] - ls[k]*np.cos(states[xi,2])/2)
+
+            # fig, ax1 = plt.subplots()   
+            # fig, ax2 = plt.subplots()
+            # print(len(X1), len(X2), len(Y1), len(Y2))
+            # ax1.plot(states[:,1], states[:,2], label=str(i+1))
+            # ax1.plot(X1,Y1,label=str(i+1))
+            plt.plot(X2,Y2,label=str(i+1))
+    
+        # ax1.xlabel("X-axis")
+        # ax2.xlabel("X-axis")
+        # ax1.ylabel("Y-axis")
+        # ax2.ylabel("Y-axis")
+        # ax1.title("trajectory of 1st rod center")
+        # ax2.title("trajectory of 2nd rod center")
+        # ax1.show()
+        # ax2.show()
+        plt.xlabel("X-axis")
+        plt.ylabel("Y-axis")
+        plt.title("trajectory of the centre of 2nd link")
+        plt.show()

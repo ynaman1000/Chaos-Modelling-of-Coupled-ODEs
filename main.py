12784c51214d4eaa74a66b3160859bd772b170b3
Naman Yadav <ynaman1000@gmail.com>
2020-06-22 22:18:47 +0530

    edited plots

--- main.py
+++ main.py
@@ -14,10 +14,10 @@ theta2_i = np.array([np.pi+0.01, np.pi, np.pi-0.1, np.pi]) # initial values of t
 p1_i = np.array([0.0, 0.0, 0.0, 0.0])                      # initial values of p1
 p2_i = np.array([0.0, 0.0, 0.0, 0.0])                      # initial values of p2
 
-mss = np.array([[1.0, 10.0], [10.0, 1.0]])       # array of mass of links, each row = [m1, m2]
-lss = np.array([[1.0, 1.0], [10.0, 1.0]])       # array of length of links, each row = [l1, l2]
+mss = np.array([[1.0, 1.0], [10.0, 1.0]])       # array of mass of links, each row = [m1, m2]
+lss = np.array([[1.0, 10.0], [10.0, 1.0]])       # array of length of links, each row = [l1, l2]
 
-stps = [0.00001, 0.0005, 0.001]          # incremental time step
+stps = [0.00001, 0.0005, 0.01]          # incremental time step
 ti = 0.0                        # initial time
 tf = 5.0                        # final time
 
@@ -25,7 +25,7 @@ tf = 5.0                        # final time
 # "I" if implicit is used
 # "E" is explicit is used
 # "rk4" if RK4 is used
-slvrs = [{"solver": "E"}, {"solver": "rk4"}]
+slvrs = [{"solver": "rk4"}, {"solver": "rk4"}]
 
 for slvr in slvrs:
     if (slvr["solver"] == "I"):             # setting up optional variables
@@ -44,8 +44,8 @@ aspct = "equal"
 
 # CAUTION: for sensible output make sure that only 1 for loop is iterating more than once
 # ........ therfore all but 1 or 2 of the following variables should be 1.
-I, J, K, L, M = 1, 1, len(stps)-1, 1, len(mss)
-nrws = M                        # number of rows of subplots
+I, J, K, L, M = 1, 1, len(stps)-1, len(lss), 1
+nrws = L                        # number of rows of subplots
 ncls = 1                        # number of columns of subplots
 
 
@@ -86,7 +86,7 @@ for i in range(0, I):             # iterating over initial conditions, ys_inits
                     plt_i = (((i*J + j)*K + k)*L + l)*M + m # index of lbls and clrs
                     states = solve(stps[k], ti, tf, slvrs[j], ys_inits[i], mss[m], lss[l])
                     if type(axs) is np.ndarray:
-                        plt_fun(axs[m], states, lss[l], mss[m], lblsm[plt_i], clrs[plt_i])
+                        plt_fun(axs[l], states, lss[l], mss[m], lblsm[plt_i], clrs[plt_i])
                     else:
                         plt_fun(axs, states, lss[l], mss[m], lblsm[plt_i], clrs[plt_i])
                         
@@ -95,8 +95,8 @@ if type(axs) is np.ndarray:
         # customized plotting, for plotting benchmark curve in all plot(s)
 	# 	uncomment next 3 lines if commented and set all the functions' arguments
         print("initial conditions: ", ys_inits[0])
-        states = solve(stps[len(stps)-1], ti, tf, slvrs[len(slvrs)-1], ys_inits[0], mss[i], lss[0])
-        plt_fun(axs[i], states, lss[0], mss[i], "M: "+str(mss[i])+" | "+str(stps[len(stps)-1])+" | "+slvrs[len(slvrs)-1]["solver"], 'k')
+        states = solve(stps[len(stps)-1], ti, tf, slvrs[len(slvrs)-1], ys_inits[0], mss[0], lss[i])
+        plt_fun(axs[i], states, lss[i], mss[0], "L: "+str(lss[i])+" | "+str(stps[len(stps)-1])+" | "+slvrs[len(slvrs)-1]["solver"], 'k')
         
         axs[i].set_xlabel(xlbl)
         axs[i].set_ylabel(ylbl)
